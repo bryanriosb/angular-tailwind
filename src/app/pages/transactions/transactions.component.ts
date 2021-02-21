5db6b5f3d009212f99debe5438b9e345043e7704
Bryan <bryan_riosb@outlook.com>
2021-02-21 18:41:42 -0500

    add communication between components

--- src/app/pages/transactions/transactions.component.ts
+++ src/app/pages/transactions/transactions.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import { Transactions } from '../../shared/interfaces/transactions';
 import { Observable } from 'rxjs';
 import { DataQueryService } from '../../shared/services/data-query.service';
+import { Input, EventEmitter, } from '@angular/core';
+import { DetailRespose } from '../../shared/interfaces/detail-response';
+
 
 @Component({
   selector: 'app-transactions',
@@ -10,12 +13,19 @@ import { DataQueryService } from '../../shared/services/data-query.service';
 })
 export class TransactionsComponent implements OnInit {
 
+  @Input() response: DetailRespose | undefined
+  @Output() transaction = new EventEmitter<Transactions>()
+
   public transactions$: Observable<Transactions[]> | undefined
 
   constructor(private dataQueryService: DataQueryService) { }
 
   ngOnInit(): void {
     this.transactions$ = this.dataQueryService.getTransactions()
+  }
+
+  getTransInfo(transaction: Transactions): void {
+    this.transaction.emit(transaction)
   }
 
 }
